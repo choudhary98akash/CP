aad77618323b721e64bc6ead43c98e03e2177141
Akash Choudhary <choudahry98akash>
2024-02-18 20:55:48 +0530

    2215. Find the Difference of Two Arrays

--- SolutionE2215.cpp
+++ SolutionE2215.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        
-        int size1 = nums1.size();
-        int size2 = nums2.size();
-        unordered_map<int,int> mp;
         vector<int> v1;
         vector<int> v2;
+        unordered_map<int,int> mp;
 
-        for(auto n : nums2)
-            mp[n] = 1;
-        
         for(auto n : nums1){
-            if(mp[n] == 1){
+            mp[n] = 1;
+        }
+       
+        for(auto n : nums2){
+            if(mp[n] ==0 ){
                 mp[n] = 2;
             }
-            else{
-                v1.push_back(n);
+            else if(mp[n] == 1){
+                mp[n] = 3;
             }
         }
+        for(auto n : mp){
+           if(n.second == 1){
+               v1.push_back(n.first);
+           }
+           else if(n.second == 2){
+               v2.push_back(n.first);
+           }
 
-        for(auto n : nums2){
-            if(mp[n] == 1)
-                v2.push_back(n);
         }
 
-    return {v1,v2};
+
+        return {v1,v2};
+
     }
 };
-//Approach has flow the elemets are repeated in array , but should not be repeated in a result array;
-
