814905f95bb3d7683e3687f236a75fd82e28c87f
Akash Choudhary <choudahry98akash>
2024-03-16 16:37:31 +0530

    Solution to problem 236

--- SolutionM236.cpp
+++ SolutionM236.cpp
@@ -61,3 +61,17 @@ public:
         return temp;
     }
 };//solution wiht 10% beter than other
+
+
+Best soutiin Goes like 
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if (!root || root == p || root == q) return root;
+    TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    TreeNode* right = lowestCommonAncestor(root->right, p, q);
+    return !left ? right : !right ? left : root;
+}
+
+//Explaination 
+Same solution in several languages. It's recursive and expands the meaning of the function. If the current (sub)tree contains both p and q, then the function result is their LCA. If only one of them is in that subtree, then the result is that one of them. If neither are in that subtree, the result is null/None/nil.
+
+Update: I also wrote two iterative solutions now, one of them being a version of the solution here. They're more complicated than this simple recursive solution, but I do find them interesting.
